4e1dcf6c9fb304000708dc1e172743d4e7557719
Din Ezra <dinezra11@gmail.com>
2021-11-13 13:56:51 +0200

    Starting Project

--- linkedlist.py
+++ linkedlist.py
@@ -1,24 +1,23 @@
 class Node:
+    """ An individual node, consists of data and a pointer to another node. """
     def __init__(self, data=None):
         self.data = data
         self.next = None
 
 
 class LinkedList:
+    """ A linked list. 'head' attribute is the beginning node of the list. """
     def __init__(self):
         self.head = None
 
     def add(self, data):
-        if self.head is None:
-            self.head = Node(data)
-        else:
-            node = self.head
-            while self.head.next is not None:
-                node = node.next
-
-            node.next = Node(data)
+        """ Add a node to the beginning of the linked list. """
+        node = Node(data)
+        node.next = self.head
+        self.head = node
 
     def size(self):
+        """ Return the size of the linked list. """
         count = 0
         node = self.head
         while node is not None:
@@ -27,7 +26,14 @@ class LinkedList:
 
         return count
 
+    def isEmpty(self):
+        if self.head is None:
+            return True
+
+        return False
+    
     def print(self):
+        """ Print the data from all of the list's nodes. """
         node = self.head
         while node is not None:
             print(node.data, " ", end="")
